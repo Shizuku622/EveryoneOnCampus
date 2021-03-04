721ea25254dc8315cd49c758eff1315a404c73ab
Shizuku622 <1369541933@qq.com>
2021-03-04 11:02:43 +0800

    2021-3-4

--- app/src/main/java/com/android/everyoneoncampus/EocApplication.java
+++ app/src/main/java/com/android/everyoneoncampus/EocApplication.java
@@ -3,9 +3,18 @@ package com.android.everyoneoncampus;
 import android.app.Application;
 import android.content.Context;
 
+import com.mysql.fabric.Server;
+
+import cn.leancloud.AVOSCloud;
+import cn.leancloud.chatkit.LCChatKit;
+
 public class EocApplication extends Application {
     private static Context context;
     private static String userID;
+
+    private final String APP_ID = "LmuXlj2lu5JzK1ynPyKwngml-gzGzoHsz";
+    private final String APP_KEY = "v4njrqhhEREfqnKj1V3VNyVT";
+    private final String SERVER_URL = "https://lmuxlj2l.lc-cn-n1-shared.com";
 
     //设置ui
     public static String getUserID() {
@@ -20,6 +29,10 @@ public class EocApplication extends Application {
     public void onCreate() {
         super.onCreate();
         context = getApplicationContext();
+
+        //leancloud init 初始化工作
+        AVOSCloud.initialize(this, APP_ID, APP_KEY, SERVER_URL);
+
     }
 
 
--- app/src/main/java/com/android/everyoneoncampus/TestActivity.java
+++ app/src/main/java/com/android/everyoneoncampus/TestActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.everyoneoncampus.allinterface.DataListener;
 import com.android.everyoneoncampus.allinterface.ReturnSQL;
@@ -15,6 +16,10 @@ import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
 
+import cn.leancloud.im.v2.AVIMClient;
+import cn.leancloud.im.v2.AVIMException;
+import cn.leancloud.im.v2.callback.AVIMClientCallback;
+
 public class TestActivity extends AppCompatActivity {
 
     MySQLModel mMySQLModel = new MySQLModel();
@@ -23,12 +28,16 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-        textView = findViewById(R.id.txt33);
-        mMySQLModel.getCurrentUserInfo(new DataListener<String>() {
-            @Override
-            public void onComplete(String result) {
-                textView.setText(result);
-            }
-        });
+
+//        AVIMClient tom = AVIMClient.getInstance("Tom");
+//        tom.open(new AVIMClientCallback() {
+//            @Override
+//            public void done(AVIMClient client, AVIMException e) {
+//                if(e == null){
+//                    Toast.makeText(TestActivity.this, "成功连接 ", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+
     }
 }
