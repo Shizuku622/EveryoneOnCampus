019b0b56615466cbffe854644111a2bea78ba1b8
Shizuku622 <1369541933@qq.com>
2021-03-27 23:50:44 +0800

    地图图层，标题栏添加菜单

--- app/src/main/java/com/android/everyoneoncampus/custom/CustomTitle.java
+++ app/src/main/java/com/android/everyoneoncampus/custom/CustomTitle.java
@@ -1,36 +1,46 @@
 package com.android.everyoneoncampus.custom;
 
 import android.content.Context;
-import android.text.Html;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.android.everyoneoncampus.R;
 
 public class CustomTitle extends RelativeLayout {
-    private ImageView mImageView ;
-    private TextView mTextView;
+    private ImageView mImgLeftExit;
+    private TextView mTxtTitle;
+    private ImageView mImgMenu;
 
     public CustomTitle(Context context, AttributeSet attrs) {
         super(context, attrs);
         LayoutInflater.from(context).inflate(R.layout.custom_title,this);
-        mImageView = findViewById(R.id.imgbtn_custom_leftexit);
-        mTextView = findViewById(R.id.txt_custom_title);
+        mImgLeftExit = findViewById(R.id.imgbtn_custom_leftexit);
+        mTxtTitle = findViewById(R.id.txt_custom_title);
+        mImgMenu = findViewById(R.id.imgbtn_custom_menu);
     }
 
+    public void setMenuEnable(boolean set){
+        if(set){
+            mImgMenu.setVisibility(View.VISIBLE);
+        }else{
+            mImgMenu.setVisibility(View.GONE);
+        }
+    }
+
+    public void setImgMenuOnClickListener(View.OnClickListener onClickListener){
+        mImgMenu.setOnClickListener(onClickListener);
+    }
 
     public void setImgBtnOnClickListener(View.OnClickListener onClickListener){
-        mImageView.setOnClickListener(onClickListener);
+        mImgLeftExit.setOnClickListener(onClickListener);
     }
 
     public void setTxtTitle(String s){
-        mTextView.setText(s);
+        mTxtTitle.setText(s);
     }
 
 }
--- app/src/main/java/com/android/everyoneoncampus/view/mainui/uifrag/uiindex/tuijian/GaodeMapActivity.java
+++ app/src/main/java/com/android/everyoneoncampus/view/mainui/uifrag/uiindex/tuijian/GaodeMapActivity.java
@@ -1,43 +1,105 @@
 package com.android.everyoneoncampus.view.mainui.uifrag.uiindex.tuijian;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.amap.api.maps.AMap;
+import com.amap.api.maps.model.MyLocationStyle;
 import com.android.everyoneoncampus.BaseActivity;
+import com.android.everyoneoncampus.R;
 import com.android.everyoneoncampus.databinding.ActivityGaodeMapBinding;
 
 public class GaodeMapActivity extends BaseActivity {
     private ActivityGaodeMapBinding mBinding;
+    private static final String TAG = "GaodeMapActivity";
+    private AMap mAMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = ActivityGaodeMapBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
+        //获得定位权限
+        setPermission();
         initView(savedInstanceState);
         initListener();
 
     }
 
+    private void setPermission() {
+        String[] strPermission = {Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION};
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+        ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,strPermission,1);
+        }
+    }
+
     private void initView(Bundle savedInstanceState) {
         mBinding.mvGaode.onCreate(savedInstanceState);
-        AMap aMap = mBinding.mvGaode.getMap();
+        mAMap = mBinding.mvGaode.getMap();
+        mAMap.showIndoorMap(true);
 
-
+        MyLocationStyle myLocationStyle = new MyLocationStyle();
+        myLocationStyle.interval(2000);
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);
+        myLocationStyle.showMyLocation(true);
+        mAMap.setMyLocationStyle(myLocationStyle);
+        mAMap.setMyLocationEnabled(true);
+        //获得定位按钮
+        mAMap.getUiSettings().setMyLocationButtonEnabled(true);
+        mAMap.addOnMyLocationChangeListener(new AMap.OnMyLocationChangeListener() {
+            @Override
+            public void onMyLocationChange(Location location) {
+                Log.d(TAG, location.getLongitude() + ","+location.getLatitude());
+            }
+        });
 
         mBinding.gaodeTitle.setTxtTitle("学校地图");
     }
 
     private void initListener() {
         mBinding.gaodeTitle.setImgBtnOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            finish();
-        }
-    });
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        mBinding.gaodeTitle.setImgMenuOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mBinding.dlMapMenu.openDrawer(GravityCompat.END);
+            }
+        });
+
+        mBinding.rgMapType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.rb_map_nav:
+                        mAMap.setMapType(AMap.MAP_TYPE_NAVI);
+                        break;
+                    case R.id.rb_map_night:
+                        mAMap.setMapType(AMap.MAP_TYPE_NIGHT);
+                        break;
+                    case R.id.rb_map_satellite:
+                        mAMap.setMapType(AMap.MAP_TYPE_SATELLITE);
+                        break;
+                }
+            }
+        });
+        mBinding.rbMapNav.setChecked(true);
     }
 
     @Override
